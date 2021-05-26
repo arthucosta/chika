2948cd28d5165707855fdb478bc6abbcbe56364d
Yslan Ramos <yslan3000@gmail.com>
2021-05-26 11:36:44 -0300

    add docs to start project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Chika
+
+To start the bot:
+
+- Install dependencies with `mix deps.get`
+- Copy the config file with `cp config/config.example.exs config/config.exs` and fill in the file
+- Start the bot with `iex mix -S`
---
+++ config/config.example.exs
@@ -0,0 +1,5 @@
+import Config
+
+config :chika,
+  riot_key: "your_riot_game_key", # get your key at https://developer.riotgames.com/
+  token: "your_discord_bot_token" # get your key at https://discord.com/developers/applications
